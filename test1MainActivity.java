ec17c1dbd1af323e353f1a647c7d0ce270364950
Hungrok <44796633+Hungrok@users.noreply.github.com>
2020-01-11 10:48:47 +0900

    Add files via upload

---
+++ test1MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.test1;
+
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Linking ViewPager - PagerAdapter
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        PagerAdapter adapter = new MyPagerAdapter(this);
+        viewPager.setAdapter(adapter);
+
+        // Button object , callback listener
+        Button bt1 = findViewById(R.id.button1);
+        Button bt2 = findViewById(R.id.button2);
+        Button bt3 = findViewById(R.id.button3);
+        Button bt4 = findViewById(R.id.button4);
+        Button bt5 = findViewById(R.id.button5);
+        Button bt6 = findViewById(R.id.button6);
+
+        // 익명함수 객체생성방법
+        View.OnClickListener ls1 = new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(1);
+            }
+        };
+        bt1.setOnClickListener(ls1);  // 핸들러에게 콜백객체 set
+
+        View.OnClickListener ls2 = new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(1);
+            }
+        };
+        bt1.setOnClickListener(ls2);
+
+        View.OnClickListener ls3 = new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(1);
+            }
+        };
+        bt1.setOnClickListener(ls3);
+
+        View.OnClickListener ls4 = new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(1);
+            }
+        };
+        bt1.setOnClickListener(ls1);
+
+        View.OnClickListener ls5 = new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(1);
+            }
+        };
+        bt1.setOnClickListener(ls1);
+
+        View.OnClickListener ls6 = new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(1);
+            }
+        };
+        bt1.setOnClickListener(ls1);
+
+
+
+
+    }
+}
---
+++ test1MyPagerAdapter.java
@@ -0,0 +1,54 @@
+package com.example.test1;
+
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class MyPagerAdapter extends PagerAdapter {
+
+
+    Context context;
+    private final int[] drawableImgs = new int[] {
+            R.drawable.ic_battery_20_black_24dp,
+            R.drawable.ic_battery_30_black_24dp,
+            R.drawable.ic_battery_50_black_24dp,
+            R.drawable.ic_battery_60_black_24dp,
+            R.drawable.ic_battery_80_black_24dp,
+            R.drawable.ic_battery_90_black_24dp
+    };
+
+    MyPagerAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return drawableImgs.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        ImageView imageView = new ImageView(context);
+        Bitmap drawImg = BitmapFactory.decodeResource(context.getResources(), drawableImgs[position]);
+
+        imageView.setImageBitmap(drawImg);
+        container.addView(imageView, 0);
+        return imageView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((ImageView) object);
+    }
+}
